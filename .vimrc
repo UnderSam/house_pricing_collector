8bf43260ab677e5497ee4a4f48e6409f825bc47f
Cheng Ju Wu <sam19970416@gmail.com>
2022-08-27 03:41:59 +0800

    clean: remove cached

--- .vimrc
+++
@@ -1,2 +0,0 @@
-nnoremap <silent><A-j> :set paste<CR>m`o<Esc>``:set nopaste<CR>
-nnoremap <silent><A-k> :set paste<CR>m`O<Esc>``:set nopaste<CR>
--- src/mongo_manager.py
+++
@@ -1,4 +0,0 @@
-import pymongo
-
-client = pymongo.MongoClient("mongodb+srv://admin:<password>@pricing-cluster.kvcsc6z.mongodb.net/?retryWrites=true&w=majority")
-db = client.test
